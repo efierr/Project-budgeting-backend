02eb4c763c10a0e7254eabeca8a03ab4c5c63379
efierr <erickfierro0413@gmail.com>
2024-07-06 12:45:48 -0400

    modified controllers and app.js     1. created routes for creating new transactions, updating, deleting ,         show a single transaction, and show all transactions.         2. added middleware to app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
-
-const cors = require("cors")
-
-
 const app = express();
 
+const cors = require("cors")
+app.use(cors());
+
+app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
 
+const transactionController = require("./controllers/transactionController");
+
+
+app.use("/transactions", transactionController);
 
 module.exports = app;
--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -19,3 +19,43 @@ router.get("/:id", (req, res) => {
         res.status(404).json({error: `no transaction with id: ${id} not found`})
     }
 })
+
+
+// create
+router.post("/", (req, res) => {
+    const newTransaction = {...req.body, id : transactionsList.length + 1};
+    transactionsList.push(newTransaction);
+    res.json(transactionsList[transactionsList.length -1 ])
+});
+
+
+// destroy 
+
+router.delete("/:id", (req, res) => {
+    const {id} = req.params;
+    const deletedTransaction = transactionsList.findIndex(transaction => transaction.id === +id);
+    if (deletedTransaction !== -1) {
+        transactionsList.splice(deletedTransaction, 1)
+        res.redirect("/transactions")
+    } else {
+        res.status(404).send({error: "no transaction found partna"})
+    }
+})
+
+
+// update 
+
+
+router.put("/:id", (req, res) => {
+    const {id} = req.params;
+    const transaction = transactionsList.findIndex(item => item.id === +id)
+
+    if (transaction !== -1 ) {
+        transactionsList[transaction] = req.body;
+        res.status(200).json(transactionsList[transaction])
+    } else {
+        res.status(404).send({error: "no transaction found partna"})
+    }
+})
+
+module.exports = router;
